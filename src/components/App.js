9b72584f16dd9a5d50148f01dd3c7e7d8a42fefa
Youngpuffy <levimuturi2@gmail.com>
2025-04-10 00:31:05 +0300

    import and export keywords

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import About from "../components/About"
-import Home from "../components/Home";
-import NavBar from "../components/NavBar";
+import About from "../components/About.js"
+import Home from "../components/Home.js";
+import NavBar from "../components/NavBar.js";
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,8 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
     </nav>
   );
 }
